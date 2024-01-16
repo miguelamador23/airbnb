3816177a729f43f5d1c26a4ae134f65bffb96abf
¨miguelamador23¨ <miguelamador23@outlook.com>
2024-01-16 08:36:03 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import './index.css'
+import Nav from './components/Nav'
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="flex items-center justify-between">
+      <div className="flex items-center">
+        <img src="logo.svg" alt="Logo" className="h-24 w-24 ml-4" />
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
