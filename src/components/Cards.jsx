05a03e9a9e1eccda743a235a1b45fabdff113510
¨miguelamador23¨ <miguelamador23@outlook.com>
2024-01-16 08:44:15 -0600

    componente card

---
+++ src/components/Cards.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Cards() {
+  return (
+    <>
+     <h1 className="text-lg font-bold ml-4">Stays in Finland</h1>
+    </>
+  );
+}
+
+export default Cards;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <nav className="flex items-center justify-between">
       <div className="flex items-center">
-        <img src="logo.svg" alt="Logo" className="h-24 w-24 ml-4" />
+        <img src="logo.svg" alt="Logo" className="h-32 w-32 ml-4" />
       </div>
     </nav>
   );
